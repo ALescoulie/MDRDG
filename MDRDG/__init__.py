e7cf96f33b7557c26a644ba85410fd96b7d54d4a
Alia Lescoulie <alia.lescoulie@gmail.com>
2022-11-14 16:39:46 -0800

    add logging and version to __init__.py

--- MDRDG/__init__.py
+++ MDRDG/__init__.py
@@ -1,7 +1,32 @@
 """A Python toolkit for running relative free energy calculations in GROMACS."""
+import logging
 
-# Add imports here
-from .mdrdg import *
+from . import log
+
+# Import core classes and methods
+from .config import Config, load_config_from_yaml
+
+# Handle versioneer
+
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
 
 
-from ._version import __version__
+def create_logger(logfile='mdrdg.log') -> logging.Logger:
+    """Starts logger"""
+    md_sapt_log = log.create('mdsapt', logfile)
+    return md_sapt_log
+
+
+def log_banner() -> None:
+    """Log program name and licence at INFO level"""
+    logger.info("MD-SAPT %s starting" % __version__)
+    logger.info("Copyright (c) 2022 Dylan Jessen, Joseph Perez, Kavana Gonur, and Alia Lescoulie")
+    logger.info("Released under GPLv3 License")
+
+
+logger = create_logger()
+log_banner()
---
+++ MDRDG/log.py
@@ -0,0 +1,27 @@
+"""
+Based on log.py https://github.com/Becksteinlab/MDPOW/blob/develop/mdpow/log.py
+Logs issues throughout MDRDG
+"""
+import logging
+
+
+def create(log_name: str, logfile: str) -> logging.Logger:
+    """
+    Create the logger for MDRDG
+    """
+    logger = logging.getLogger(log_name)
+
+    logger.setLevel(logging.DEBUG)
+
+    logfile = logging.FileHandler(logfile)
+    logger_formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    logfile.setFormatter(logger_formatter)
+    logger.addHandler(logfile)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+    return logger

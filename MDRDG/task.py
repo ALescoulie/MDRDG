6190c42c6f1ecfa99b26b9061d832ca206b51581
Alia Lescoulie <alia.lescoulie@gmail.com>
2022-11-20 13:21:41 -0800

    add functions for encoding and decoding tasks to json format

--- MDRDG/task.py
+++ MDRDG/task.py
@@ -4,7 +4,9 @@ r"""
 from collections import defaultdict
 from pathlib import Path
 from enum import Enum
-from typing import List, Union, Type, NamedTuple, Dict, Tuple, Optional
+from typing import List, Union, Type, NamedTuple, Dict, Tuple, Optional, Any, Set
+
+from .config import Config
 
 
 class Operations(Enum):
@@ -48,9 +50,30 @@ Step: Type = Union[ConversionSteps, MappingSteps, TopGenSteps, LambdaSteps, FepS
 class Task(NamedTuple):
     step: int
     operation: Operations
-    step: Step
+    op_step: Step
     files: List[Path]
     directory: Path
+
+
+def encode_task(task: Task) -> Dict[str, Any]:
+    task_dict = {
+        'step': task.op_step,
+        'op': task.operation.value,
+        'op_step': task.op_step.value,
+        'files': [str(p) for p in task.files],
+        'dir': str(task.directory)
+    }
+    return task_dict
+
+
+def decode_task(task_dict: Dict[str, Any]) -> Task:
+    # TODO fix encoding to use enum operations
+    task: Task = Task(task_dict['step'],
+                      Operations[task_dict['op']],
+                      task_dict['op_step'],
+                      task_dict['files'],
+                      task_dict['dir'])
+    return task
 
 
 def run_task(task: Task) -> None:
@@ -58,26 +81,29 @@ def run_task(task: Task) -> None:
 
 
 class TaskQueue:
-    _tasks: List[Task]
+    _tasks: Set[Task]
     _batch_list = List[List[Task]]
-    _n_cur: int
 
     def __init__(self, tasks: List[Task]) -> None:
-        self._tasks = tasks
+        self._tasks = set(tasks)
+        self._batch_sort()
 
-    def _build_batches(self) -> None:
+    def _batch_sort(self) -> None:
         batch_dict: Dict[Tuple[int, int], List[Task]] = defaultdict(list)
 
         for task in self._tasks:
-            batch_dict[(task.operation.value, task.step.value)].append(task)
+            batch_dict[(task.operation.value, task.op_step.value)].append(task)
 
         batch_keys: List[Tuple[int, int]] = list(batch_dict.keys())
         self._batch_list = [batch_dict[k] for k in sorted(batch_keys, key=lambda x: (x[0], x[1]))]
 
-    def pop_batch(self) -> Optional[List[Task]]:
-        if self._n_cur == len(self._batch_list):
+    def pop(self) -> Optional[List[Task]]:
+        if len(self._batch_list) == 0:
             return None
         else:
-            batch: List[Task] = self._batch_list[self._n_cur]
-            self._n_cur += 1
+            batch: List[Task] = self._batch_list.pop(0)
             return batch
+
+    def enqueue(self, batch: List[Task]) -> None:
+        self._tasks += batch
+        self._batch_sort()
